3cc01242d9055309f91d0d1a891ca07c980de59d
Guilherme Garcia <garciaguig@gmail.com>
2025-06-12 14:07:16 -0300

    feat: Implement initial routes structure for 'complaints'

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import errorMiddleware from './middlewares/error-middleware'
+import routes from './routes'
 
 const app = express()
 
@@ -17,7 +18,9 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.json({ message: 'API is running' })
 })
+app.use('/api', routes)
 
+// Error handling middleware should be last
 app.use(errorMiddleware)
 
 const PORT = process.env.API_PORT || 3000
