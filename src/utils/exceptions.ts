ea3538ba4fc75992dff0e56ee15dc931576ed456
Guilherme Garcia <garciaguig@gmail.com>
2025-06-11 23:18:25 -0300

    chore: Create 'exceptions' util

---
+++ src/utils/exceptions.ts
@@ -0,0 +1,24 @@
+abstract class HttpException extends Error {
+  status: number
+
+  constructor(status: number, message: string) {
+    super(message)
+    this.status = status
+  }
+}
+
+export class BadRequestException extends HttpException {
+  private static status = 400
+
+  constructor(message: string) {
+    super(BadRequestException.status, message)
+  }
+}
+
+export class NotFoundException extends HttpException {
+  private static status = 404
+
+  constructor(message: string) {
+    super(NotFoundException.status, message)
+  }
+}
