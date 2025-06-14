bc81f2455f75cda68e5ba76efc953f15c04743d3
Guilherme Garcia <garciaguig@gmail.com>
2025-06-13 21:39:28 -0300

    chore: Implement 'verifyRegisterToken' on jwt util

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,5 +1,6 @@
 import { UserBasicInfo } from '@/types/user'
-import jwt, { SignOptions } from 'jsonwebtoken'
+import jwt, { SignOptions, TokenExpiredError } from 'jsonwebtoken'
+import { BadRequestException, UnauthorizedException } from './exceptions'
 
 const JWT_SECRET = process.env.JWT_SECRET!
 const JWT_ALGORITHM = 'HS256'
@@ -34,6 +35,19 @@ export const generateRegisterToken = (email: string): string => {
   return token
 }
 
-export const verifyToken = (token: string): UserBasicInfo => {
-  return jwt.verify(token, JWT_SECRET) as UserBasicInfo
+export const verifyRegisterToken = (token: string | null): string => {
+  if (!token) {
+    throw new BadRequestException('Token de verificação não encontrado')
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { email: string }
+    return decoded.email
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      throw new UnauthorizedException('Token de verificação expirado')
+    }
+
+    throw new UnauthorizedException('Token de verificação inválido')
+  }
 }
