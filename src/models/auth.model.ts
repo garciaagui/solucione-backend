ef0e1b77ef4bdb66b7659226b6c57941552a0c8a
Guilherme Garcia <garciaguig@gmail.com>
2025-06-13 21:04:20 -0300

    feat: Implement create and update user methods on 'auth' model

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,3 +1,4 @@
+import { CreateUserData, UpdateUserData } from '@/types/auth'
 import { PrismaClient, User } from '@prisma/client'
 
 export default class AuthModel {
@@ -6,6 +7,19 @@ export default class AuthModel {
   public async findUserByEmail(email: string): Promise<User | null> {
     return this.prisma.user.findUnique({
       where: { email },
+    })
+  }
+
+  public async createUser(data: CreateUserData): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    })
+  }
+
+  public async updateUser(email: string, data: UpdateUserData): Promise<User> {
+    return this.prisma.user.update({
+      where: { email },
+      data,
     })
   }
 }
