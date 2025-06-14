0185a592432870237ade0a448ad3fb13d3dc503d
Guilherme Garcia <garciaguig@gmail.com>
2025-06-13 21:02:17 -0300

    chore: Implement 'generateRegisterToken' on jwt util

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,18 +2,32 @@ import { UserBasicInfo } from '@/types/user'
 import jwt, { SignOptions } from 'jsonwebtoken'
 
 const JWT_SECRET = process.env.JWT_SECRET!
-const JWT_EXPIRE_TIME = process.env.JWT_EXPIRE_TIME as SignOptions['expiresIn']
+const JWT_ALGORITHM = 'HS256'
 
-export const generateToken = (user: UserBasicInfo): string => {
+export const generateLoginToken = (user: UserBasicInfo): string => {
   const jwtConfig: SignOptions = {
-    expiresIn: JWT_EXPIRE_TIME,
-    algorithm: 'HS256',
+    expiresIn: '24h',
+    algorithm: JWT_ALGORITHM,
   }
 
   const payload = {
     id: user.id,
     email: user.email,
     name: user.name,
+  }
+
+  const token = jwt.sign(payload, JWT_SECRET, jwtConfig)
+  return token
+}
+
+export const generateRegisterToken = (email: string): string => {
+  const jwtConfig: SignOptions = {
+    expiresIn: '10m',
+    algorithm: JWT_ALGORITHM,
+  }
+
+  const payload = {
+    email,
   }
 
   const token = jwt.sign(payload, JWT_SECRET, jwtConfig)
