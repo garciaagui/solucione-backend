e00cda6f8f3e0e6afb3288bed4842fdf3ee38eb4
Guilherme Garcia <garciaguig@gmail.com>
2025-06-28 16:07:37 -0300

    chore: Add new prisma migration

---
+++ prisma/migrations/20250628183153_drop_id_default/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "complaints" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "replies" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "id" DROP DEFAULT;
