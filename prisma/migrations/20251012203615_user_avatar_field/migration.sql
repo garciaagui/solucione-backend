37d242b7a44f33ac70917241d99d2941a8bf5c4d
Guilherme Garcia <garciaguig@gmail.com>
2025-10-29 09:17:24 -0300

    chore: update avatar urls on user avatar field migration

--- prisma/migrations/20251012203615_user_avatar_field/migration.sql
+++ prisma/migrations/20251012203615_user_avatar_field/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "users" ADD COLUMN     "avatar" VARCHAR(255);
 
 -- Update existing users' avatar URLs
-UPDATE "users" SET "avatar" = 'https://api.dicebear.com/9.x/thumbs/svg?seed=Brian'     WHERE "email" = 'joao@example.com';
-UPDATE "users" SET "avatar" = 'https://api.dicebear.com/9.x/thumbs/svg?seed=Maria'     WHERE "email" = 'maria@example.com';
-UPDATE "users" SET "avatar" = 'https://api.dicebear.com/9.x/thumbs/svg?seed=Mason'     WHERE "email" = 'carlos@example.com';
-UPDATE "users" SET "avatar" = 'https://api.dicebear.com/9.x/thumbs/svg?seed=Alexander' WHERE "email" = 'ana@example.com';
+UPDATE "users" SET "avatar" = 'https://api.dicebear.com/9.x/thumbs/svg?seed=joao@example.com'     WHERE "email" = 'joao@example.com';
+UPDATE "users" SET "avatar" = 'https://api.dicebear.com/9.x/thumbs/svg?seed=maria@example.com'     WHERE "email" = 'maria@example.com';
+UPDATE "users" SET "avatar" = 'https://api.dicebear.com/9.x/thumbs/svg?seed=carlos@example.com'     WHERE "email" = 'carlos@example.com';
+UPDATE "users" SET "avatar" = 'https://api.dicebear.com/9.x/thumbs/svg?seed=ana@example.com' WHERE "email" = 'ana@example.com';
