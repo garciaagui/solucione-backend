83158ddf17a798d7d884d07bcece776d7eb46524
Guilherme Garcia <garciaguig@gmail.com>
2025-11-08 16:04:19 -0300

    feat: implement reply creation admin route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import authRoutes from './auth.routes'
 import complaintRoutes from './complaint.routes'
+import replyRoutes from './reply.routes'
 
 const router = Router()
 
 router.use('/auth', authRoutes)
 router.use('/complaints', complaintRoutes)
+router.use('/admin/replies', replyRoutes)
 
 export default router
