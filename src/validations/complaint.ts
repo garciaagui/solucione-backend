1bd27b7f0fc76dea0dae78effd5288a2cb9426c9
Guilherme Garcia <garciaguig@gmail.com>
2025-11-08 16:02:55 -0300

    feat: implement reply creation and complaint status update validations

--- src/validations/complaint.ts
+++ src/validations/complaint.ts
@@ -1,11 +1,24 @@
 import { createComplaintSchema } from '@/schemas/complaint'
 import { CreateComplaintRequest } from '@/types/complaint'
 import { BadRequestException } from '@/utils/exceptions'
+import { Status } from '@prisma/client'
 
 export const validateComplaintCreation = (data: CreateComplaintRequest['text']) => {
   const { error } = createComplaintSchema.validate(data)
 
   if (error) {
     throw new BadRequestException(error.message)
+  }
+}
+
+export const validateComplaintStatusForUpdate = (status: Status) => {
+  if (status === 'Finalizado') {
+    throw new BadRequestException('Reclamação já finalizada')
+  }
+
+  if (status !== 'Aberto' && status !== 'Andamento') {
+    throw new BadRequestException(
+      'Apenas reclamações com status Aberto ou Andamento podem ser atualizadas',
+    )
   }
 }
